3f5f5faf9ee4083e36e1abfa2cd27a52f6452996
Azza Ahmed <azzaea@uofk.edu>
2016-08-11 18:08:18 +0300

    adding the pipe function

--- align.swift
+++ align.swift
@@ -1,31 +1,36 @@
 import sys;
 import files;
 import string;
-import io;
+
 /////////////////////////////////// Pipeline functions definition
 
 /////// Alignment functions:
+@dispatch=WORKER
 app (file output) bwa (string read1, string read2, string INDEX, string bwamemparams, int PBSCORES,  string rgheader){
 	"/usr/bin/bwa" "mem" "-M" bwamemparams "-t" PBSCORES "-R" rgheader  INDEX read1 read2 @stdout=output;
 }
 
+@dispatch=WORKER
 app (file output) novoalign (string read1, string read2, string INDEX, string novoalignparams, int PBSCORES,  string rgheader) {
 	"/usr/local/src/novocraft/novoalign" novoalignparams "-c" PBSCORES "-d" INDEX "-f" read1 read2 @stdout=output;
 }
 
+@dispatch=WORKER
 app (file output) samblaster(string inputFilename){
 	"samblaster" "-M" inputFilename @stdout=output;
 }
 
-app (file output) samtools(string inputFilename, int thr, string u){
+@dispatch=WORKER
+app (file output) samtools.view(string inputFilename, int thr, string u){
 	"samtools" "view" "-@" thr "-bS" u inputFilename @stdout=output;
 }
 
+@dispatch=WORKER
 app (file output) novosort (string inputFilename, string tmpdir, int thr, string sortoptions){
 	"novosort" "--index" "--tmpdir" tmpdir "--threads" thr sortoptions inputFilename "-o" output;
 }
 
-
+@dispatch=WORKER
 app () picard (string java, string picard, string tmpdir, string inputFilename, string outputfile, string metricsfile){
 //      java "-Xmx8g" "-Djava.io.tmpdir" tmpdir "-jar" picard "MarkDuplicates"  "INPUT" inputFilename "OUTPUT" output "TMP_DIR" tmpdir "ASSUME_
         java "-jar" picard "MarkDuplicates"  inputFilename outputfile metricsfile ;
@@ -71,6 +76,8 @@ foreach sample in sampleLines{
 	string read1 = sampleInfo[1];
 	string read2 = sampleInfo[2];
 
+	///////////////// Alignment deduplication (per sample):
+
 	string rgheader = sprintf("@RG\tID:%s\tLB:%s\tPL:%s\tPU:%s\tSM:%s\tCN:%s\t", sampleName, vars["SAMPLELB"], vars["SAMPLEPL"], sampleName, sampleName, vars["SAMPLECN"]);
 
 	file alignedsam <strcat(vars["TMPDIR"],"/align/", sampleName, ".nodups.sam")>;
@@ -79,43 +86,41 @@ foreach sample in sampleLines{
 	file dedupbam <strcat(vars["OUTPUTDIR"], "/align/", sampleName, ".wdups.bam")>;
 	file dedupsortedbam <strcat(vars["OUTPUTDIR"], "/align/", sampleName, ".wdups.sorted.bam")>;
 	file alignedsortedbam <strcat(vars["OUTPUTDIR"], "/align/", sampleName, ".nodups.sorted.bam")>;
+	file metricsfile <strcat(vars["OUTPUTDIR"], "/align/", sampleName, ".picard.metrics")>;
+	file flagstats <strcat(filename(dedupsortedbam), ".flagstats">;
 
-
-	//file sortedbam<SingleFileMapper; file=strcat(parameters.OUTPUTDIR,"/align/", sampleName, ".nodups.sorted.bam")>;
-
-
-		switch(vars["MARKDUPLICATESTOOL"]){
+	switch(vars["MARKDUPLICATESTOOL"]){
 		case "SAMBLASTER":
 			alignedsam = bwa(read1, read2, vars["BWAINDEX"], vars["BWAMEMPARAMS"], string2int(vars["PBSCORES"]), rgheader);
 			dedupsam = samblaster(filename(alignedsam));
-			dedupbam = samtools(filename(dedupsam), string2int(vars["PBSCORES"]),"-u");
+			dedupbam = samtools.view(filename(dedupsam), string2int(vars["PBSCORES"]),"-u");
 			dedupsortedbam = novosort(filename(dedupbam),vars["TMPDIR"], string2int(vars["PBSCORES"]), "--compression 1");
-
+			
 		case "NOVOSORT":
 			switch (vars["ALIGNERTOOL"])
 			{
 			case "BWAMEM":
 				alignedsam = bwa(read1, read2, vars["BWAINDEX"], vars["BWAMEMPARAMS"], string2int(vars["PBSCORES"]), rgheader);
-				alignedbam = samtools(filename(alignedsam), string2int(vars["PBSCORES"]), "-u");
+				alignedbam = samtools.view(filename(alignedsam), string2int(vars["PBSCORES"]), "-u");
 	                case "NOVOALIGN":
 				alignedsam = novoalign(read1, read2, strcat(vars["REFGENOMEDIR"],"/",vars["NOVOALIGNINDEX"]), vars["NOVOALIGNPARAMS"], string2int(vars["PBSCORES"]));
-				alignedbam = samtools(filename(alignedsam), string2int(vars["PBSCORES"])," ");
+				alignedbam = samtools.view(filename(alignedsam), string2int(vars["PBSCORES"])," ");
 			}
 			dedupsortedbam = novosort(filename(alignedbam),vars["TMPDIR"], string2int(vars["PBSCORES"]), strcat("--markDuplicates -r",rgheader) );
 
 		case "PICARD":
-			file dedupsortedbam =  touch(filename(dedupsortedbam));
-                        file metrics <strcat(vars["OUTPUTDIR"], "/align/", sampleName, ".picard.metrics")>;
+			dedupsortedbam =  touch(filename(dedupsortedbam));
                         metricsfile = touch(filename(metrics));	
 
 			alignedsam = bwa(read1, read2, vars["BWAINDEX"], vars["BWAMEMPARAMS"], string2int(vars["PBSCORES"]), rgheader);
-			alignedbam = samtools(filename(alignedsam), string2int(vars["PBSCORES"]), "-u");
+			alignedbam = samtools.view(filename(alignedsam), string2int(vars["PBSCORES"]), "-u");
 			alignedsortedbam = novosort(filename(alignedbam),vars["TMPDIR"], string2int(vars["PBSCORES"])," ");                  
-			picard(vars["JAVADIR"], vars["PICARDIR"], vars["TMPDIR"], strcat("INPUT=",filename(alignedsortedbam)), strcat("OUTPUT=",filename(dedupsortedbam)), strcat("METRICS_FILE=",filename(metricsfile))   ) ;
- 
+			picard(vars["JAVADIR"], vars["PICARDIR"], vars["TMPDIR"], strcat("INPUT=",filename(alignedsortedbam)), strcat("OUTPUT=",filename(dedupsortedbam)), strcat("METRICS_FILE=",filename(metricsfile))   ) ; 
 	}
 
-start from line 568 in align_dedup.sh
+	///////////////// Alignment QC (per sample):
+	//start from line 568 in align_dedup.sh
+
 
 }
 
---
+++ pipe.c
@@ -0,0 +1,87 @@
+/* pipe demo */ /* Paul Krzyzanowski */ 
+// from https://www.cs.rutgers.edu/~pxk/416/notes/c-tutorials/pipe.html
+//
+
+#include <stdlib.h> 
+#include <stdio.h> 
+#include <string.h>
+
+
+#define OK       0
+#define NO_INPUT 1
+#define TOO_LONG 2
+#define CMD_LENGTH 30
+
+
+void runpipe(); 
+static int getLine (char *buff, size_t sz) ;
+
+
+int main(int argc, char **argv) 
+{
+	int pid, status; 
+	int fd[2]; 
+//	char *cmd1[] = { "/bin/ls", "-al", "/", 0 }; 
+//	char *cmd2[] = { "/usr/bin/tr", "a-z", "A-Z", 0 }; 
+// reading user's commands:
+        int rc;
+//        char cmd1[CMD_LENGTH], cmd2[CMD_LENGTH] ;
+	char *cmd1[] = { "/bin/ls", "-al", "/", 0 };
+	char *cmd2[] = { "/usr/bin/tr", "a-z", "A-Z", 0 };
+ //       rc = getLine (cmd1, sizeof(cmd1));
+//        rc = getLine (cmd2, sizeof(cmd2));                
+///  	printf("%s",&cmd1[0])      ;
+	pipe(fd); 
+	switch (pid = fork()) { 
+		case 0: /* child */ 
+			runpipe(fd, &cmd1, &cmd2); 
+			exit(0); 
+		default: /* parent */ 
+			while ((pid = wait(&status)) != -1) 
+				fprintf(stderr, "process %d exits with %d\n", pid, WEXITSTATUS(status)); 
+			break; 
+		case -1: 
+			perror("fork"); 
+			exit(1); 
+	} 
+	exit(0);
+}
+
+
+void runpipe(int pfd[], char *cmd1, char *cmd2) {
+     	int pid; 
+	switch (pid = fork()) { 
+	       	case 0: /* child */ 
+			dup2(pfd[0], 0); 
+			close(pfd[1]); /* the child does not need this end of the pipe */ 
+			execvp(cmd2[0], cmd2); 
+			perror(cmd2[0]); 
+		default: /* parent */ 
+			dup2(pfd[1], 1); 
+			close(pfd[0]); /* the parent does not need this end of the pipe */ 
+			execvp(cmd1[0], cmd1); 
+			perror(cmd1[0]); 
+		case -1: 
+//			perror("fork"); 
+			exit(1); 
+	} 
+}
+
+static int getLine (char *buff, size_t sz) {
+            int ch, extra;
+            if (fgets (buff, sz, stdin) == NULL)
+                return NO_INPUT;
+// If it was too long, there'll be no newline. In that case, we flush
+// to end of line so that excess doesn't affect the next call.
+            if (buff[strlen(buff)-1] != '\n') {
+                extra = 0;
+                while (((ch = getchar()) != '\n') && (ch != EOF))
+                        extra = 1;
+                return (extra == 1) ? TOO_LONG : OK;
+            }       
+// Otherwise remove newline and give string back to caller.
+             buff[strlen(buff)-1] = '\0';
+             return OK;
+}
+
+
