398c34886e28f3529539ea649d7e3494f9b68ace
Elliott Rodriguez <edrodri2@illinois.edu>
2016-08-11 10:58:58 -0500

    pipe update

--- pipe.c
+++ pipe.c
@@ -1,9 +1,9 @@
-/* pipe demo */ /* Paul Krzyzanowski */ 
+/* pipe demo */ /* Paul Krzyzanowski */
 // from https://www.cs.rutgers.edu/~pxk/416/notes/c-tutorials/pipe.html
 //
 
-#include <stdlib.h> 
-#include <stdio.h> 
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 #define OK       0
@@ -11,55 +11,57 @@
 #define TOO_LONG 2
 #define CMD_LENGTH 30
 
-void runpipe(); 
+void runpipe();
 static int getLine (char *buff, size_t sz) ;
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
-	int pid, status; 
-	int fd[2]; 
+	char ** cmd1;
+	char ** cmd2;
+	if(argc == 10){
+		cmd1 = { argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7], argv[8] };
+		cmd2 = &argv[9];
+	}
+	int pid, status;
+	int fd[2];
 // reading user's commands:
-        int rc;
-//        char cmd1[CMD_LENGTH], cmd2[CMD_LENGTH] ;
-	char *cmd1[] = { "/bin/ls", "-al", "/", 0 };
-	char *cmd2[] = { "/usr/bin/tr", "a-z", "A-Z", 0 };
- //       rc = getLine (cmd1, sizeof(cmd1));
-//        rc = getLine (cmd2, sizeof(cmd2));                
-///  	printf("%s",&cmd1[0])      ;
-	pipe(fd); 
-	switch (pid = fork()) { 
-		case 0: /* child */ 
-			runpipe(fd, cmd1, cmd2); 
-			exit(0); 
-		default: /* parent */ 
-			while ((pid = wait(&status)) != -1) 
-				fprintf(stderr, "process %d exits with %d\n", pid, WEXITSTATUS(status)); 
-			break; 
-		case -1: 
-			perror("fork"); 
-			exit(1); 
-	} 
+
+
+
+	pipe(fd);
+	switch (pid = fork()) {
+		case 0: /* child */
+			runpipe(fd, cmd1, cmd2);
+			exit(0);
+		default: /* parent */
+			while ((pid = wait(&status)) != -1)
+				fprintf(stderr, "process %d exits with %d\n", pid, WEXITSTATUS(status));
+			break;
+		case -1:
+			perror("fork");
+			exit(1);
+	}
 	exit(0);
 }
 
 
 void runpipe(int pfd[], char *cmd1[], char *cmd2[]) {
-     	int pid; 
-	switch (pid = fork()) { 
-	       	case 0: /* child */ 
-			dup2(pfd[0], 0); 
-			close(pfd[1]); /* the child does not need this end of the pipe */ 
-			execvp(cmd2[0], cmd2); 
-			perror(cmd2[0]); 
-		default: /* parent */ 
-			dup2(pfd[1], 1); 
-			close(pfd[0]); /* the parent does not need this end of the pipe */ 
-			execvp(cmd1[0], cmd1); 
-			perror(cmd1[0]); 
-		case -1: 
-//			perror("fork"); 
-			exit(1); 
-	} 
+     	int pid;
+	switch (pid = fork()) {
+	       	case 0: /* child */
+			dup2(pfd[0], 0);
+			close(pfd[1]); /* the child does not need this end of the pipe */
+			execvp(cmd2[0], cmd2);
+			perror(cmd2[0]);
+		default: /* parent */
+			dup2(pfd[1], 1);
+			close(pfd[0]); /* the parent does not need this end of the pipe */
+			execvp(cmd1[0], cmd1);
+			perror(cmd1[0]);
+		case -1:
+//			perror("fork");
+			exit(1);
+	}
 }
 
 static int getLine (char *buff, size_t sz) {
@@ -73,10 +75,8 @@ static int getLine (char *buff, size_t sz) {
                 while (((ch = getchar()) != '\n') && (ch != EOF))
                         extra = 1;
                 return (extra == 1) ? TOO_LONG : OK;
-            }       
+            }
 // Otherwise remove newline and give string back to caller.
              buff[strlen(buff)-1] = '\0';
              return OK;
 }
-
-
