12aee433cd3931d48d0f9f45767e58c5ed54e6a3
Azza Ahmed <azzaea@uofk.edu>
2016-08-30 22:32:20 +0300

    dispatching workers

--- pipelinefunctions/alignment.swift
+++ pipelinefunctions/alignment.swift
@@ -1,31 +1,31 @@
 /////// Alignment functions:
-//@dispatch=WORKER
+@dispatch=WORKER
 app (file output) bwa (string bwadir, string read1, string read2, string INDEX, string bwamemparams, int PBSCORES,  string rgheader){
 	bwadir "mem" "-M" bwamemparams "-t" PBSCORES "-R" rgheader  INDEX read1 read2 @stdout=output;
 }
 
-//@dispatch=WORKER
+@dispatch=WORKER
 app (file output) novoalign (string novoaligndir, string read1, string read2, string INDEX, string novoalignparams, int PBSCORES, string rgheader) {
 //	novoaligndir novoalignparams "-c" PBSCORES "-d" INDEX "-f" read1 read2 "-o" "SAM" rgheader @stdout=output;
 	novoaligndir novoalignparams "-c" PBSCORES "-d" INDEX "-f" read1 read2 "-o" "SAM" rgheader @stdout=output;
 }
 
-//@dispatch=WORKER
+@dispatch=WORKER
 app (file output) samtools_view(string samtoolsdir, file inputFilename, int thr, string u){
 	samtoolsdir "view" "-@" thr "-bS" u inputFilename @stdout=output;
 }
 
-//@dispatch=WORKER
+@dispatch=WORKER
 app (file output) samblaster(string samblasterdir, string inputFilename){
 	samblasterdir "-M" inputFilename @stdout=output;
 }
 
-//@dispatch=WORKER
+@dispatch=WORKER
 app (file output) novosort (string novosortdir, file inputFilename, string tmpdir, int thr, string sortoptions){
 	novosortdir "--index" "--tmpdir" tmpdir "--threads" thr inputFilename "-o" output  sortoptions;
 }
 
-//@dispatch=WORKER
+@dispatch=WORKER
 app (file outputfile, file metricsfile) picard (string javadir, string picarddir, string tmpdir, file inputFilename ){
         javadir "-jar" picarddir "MarkDuplicates"  "INPUT=" inputFilename "OUTPUT=" outputfile "METRICS_FILE=" metricsfile;
 
@@ -35,7 +35,7 @@ app (file outputfile, file metricsfile) picard (string javadir, string picarddir
 
 ////////////////////////////////////////////////////////////// Testing and validation functions:
 
-//@dispatch=WORKER
+@dispatch=WORKER
 app (file output) samtools_flagstat(string samtoolsdir, string inputFilename){
 	samtoolsdir "flagstat" inputFilename @stdout=output;
 }
