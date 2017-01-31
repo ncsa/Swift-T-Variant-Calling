5a3effb7544b726a89d23bf0e1bf1277afd70f44
Jacob Heldenbrand <jacobrh91@gmail.com>
2017-01-31 11:12:01 -0600

    remove old folder

--- pipelinefunctions/joint_vcf.swift
+++
@@ -1,6 +0,0 @@
-@dispatch=WORKER
-app (file outputfile, file outLog) GenotypeGVCFs (string javadir, string gatkdir, string reference, string variants[] ) {
-        javadir "-Xmx8g" "-jar" gatkdir "-T" "GenotypeGVCFs" "-R" reference  variants "-o" outputfile @stderr=outLog;
-}
-
-
