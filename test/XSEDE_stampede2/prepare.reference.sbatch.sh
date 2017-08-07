60388893e3e3120b5461baba5393a117d92566d2
Azza Ahmed <tg838247@login4.stampede2.tacc.utexas.edu>
2017-08-07 07:55:25 -0500

    first test on stampede2

--- test/XSEDE_stampede2/prepare.reference.sbatch.sh
+++ test/XSEDE_stampede2/prepare.reference.sbatch.sh
@@ -28,7 +28,7 @@
 #SBATCH -p normal          # Queue (partition) name
 #SBATCH -N 1               # Total # of nodes (must be 1 for serial)
 #SBATCH -n 1               # Total # of mpi tasks (should be 1 for serial)
-#SBATCH -t 02:30:00        # Run time (hh:mm:ss)
+#SBATCH -t 12:30:00        # Run time (hh:mm:ss)
 #SBATCH --mail-user=azzaea@gmail.com
 #SBATCH --mail-type=all    # Send email at begin and end of job
 
@@ -37,7 +37,7 @@
 bwa=/home1/04525/tg838247/software/bwa/bwa-0.7.16/bwa
 novocraft=/home1/04525/tg838247/software/novocraft/novocraft-3.08.00/
 samtools=/home1/04525/tg838247/software/samtools/samtools-1.5/samtools
-picardjar=/home1/04525/tg838247/software/picard/picard-2.10.5/picard.jar
+picardjar=/home1/04525/tg838247/software/picard/picard-2.10.7/picard.jar
 java=/bin/java
 gatkjar=/home1/04525/tg838247/software/gatk/GenomeAnalysisTK-3.8-0-ge9d806836/GenomeAnalysisTK.jar
 refdir=/work/04525/tg838247/stampede2/swift_T_project/data/genome/gatk-bundle/b37
@@ -47,13 +47,13 @@ set -x
 
 cd $refdir
 
-gunzip human_g1k_v37.fasta.gz
-gunzip human_g1k_v37.fasta.fai.gz
-gunzip human_g1k_v37.dict.gz
+#gunzip human_g1k_v37.fasta.gz
+#gunzip human_g1k_v37.fasta.fai.gz
+#gunzip human_g1k_v37.dict.gz
 
-$bwa index -a bwtsw human_g1k_v37.fasta 
-$novocraft/index human_g1k_v37.nix human_g1k_v37.fasta 
-$samtools faidx human_g1k_v37.fasta
+#$bwa index -a bwtsw human_g1k_v37.fasta 
+#$novocraft/novoindex human_g1k_v37.nix human_g1k_v37.fasta 
+#$samtools faidx human_g1k_v37.fasta
 
 $java -jar $picardjar CreateSequenceDictionary \
 	REFERENCE=human_g1k_v37.fasta \
