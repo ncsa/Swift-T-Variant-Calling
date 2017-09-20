675846745831527f5cf04ffc13cb444b53673d94
zo0z <azzaea@uofk.edu>
2017-09-20 07:15:05 -0500

    testing on XSEDE files

--- test/XSEDE_stampede2/tst-settings.sh
+++ test/XSEDE_stampede2/tst-settings.sh
@@ -2,17 +2,11 @@ export PROCS=12 #total number of MPI processes
 export PPN=3 #Number of processes per node 
 export NODES=4
 export QUEUE=normal
-export WALLTIME=24:00:00
+export WALLTIME=48
 export TURBINE_LOG=1
 export MAIL_ENABLED=1
 export MAIL_ADDRESS=azzaea@gmail.com
-#export ADLB_PERF_COUNTERS=1
-# the variables below didn't really chip in much!
-#export  ADLB_REPORT_LEAKS=1
-#export  ADLB_TRACE=true
-#export ADLB_DEBUG=true 
 export  ADLB_DEBUG_RANKS=1
 export TURBINE_APP_RETRIES=100
-export SWIFT_TMP=/scratch/04525/tg838247/swift_T_project/tmp_swift
 
 
