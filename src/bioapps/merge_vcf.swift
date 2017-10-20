b8dc107165b320a09b481fabe5c166a7b022cf95
Jacob Heldenbrand <jacobrh91@gmail.com>
2017-10-20 13:49:05 -0500

    Added java heap flag

--- src/bioapps/merge_vcf.swift
+++ src/bioapps/merge_vcf.swift
@@ -1,6 +1,6 @@
 @dispatch=WORKER
-app (file outputfile, file outLog) CombineGVCFs (string javaexe, string gatkjar, string reference, string dbsnp, string variants[] ) {
-        javaexe "-Xmx16g" "-jar" gatkjar "-T" "CombineGVCFs" "-R" reference "--dbsnp" dbsnp variants "-o" outputfile @stderr=outLog;
+app (file outputfile, file outLog) CombineGVCFs (string javaexe, string java_heap, string gatkjar, string reference, string dbsnp, string variants[] ) {
+        javaexe java_heap "-jar" gatkjar "-T" "CombineGVCFs" "-R" reference "--dbsnp" dbsnp variants "-o" outputfile @stderr=outLog;
 }
 
 
