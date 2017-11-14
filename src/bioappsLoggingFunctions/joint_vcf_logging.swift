81a0d675331e7598d02c67e9adbdbe4d3c05d292
zo0z <azzaea@uofk.edu>
2017-11-13 19:11:24 -0600

    fixing a logging typo

--- src/bioappsLoggingFunctions/joint_vcf_logging.swift
+++ src/bioappsLoggingFunctions/joint_vcf_logging.swift
@@ -3,9 +3,9 @@
 import bioapps.joint_vcf;
 
 (file outputfile, file outLog, file tmptimeLog) GenotypeGVCFs_logged (string javaexe, string java_heap, string gatkjar, string reference, string variants[], string threads) {
-	string startmsg = strcat("ALL", "\t", "ALL", "\t", "GenotypeGVCFs start", "\t", toString(clock_seconds()), "jointGenotypeRun\n") =>
+	string startmsg = strcat("ALL", "\t", "ALL", "\t", "GenotypeGVCFs start", "\t", toString(clock_seconds()), "\tjointGenotypeRun\n") =>
 	outputfile, outLog = GenotypeGVCFs (javaexe, strcat("-Xmx", java_heap),  gatkjar, reference, variants, threads) =>
-	string endmsg = strcat("ALL", "\t", "ALL", "\t", "GenotypeGVCFs end", "\t", toString(clock_seconds()), "jointGenotypeRun\n") =>
+	string endmsg = strcat("ALL", "\t", "ALL", "\t", "GenotypeGVCFs end", "\t", toString(clock_seconds()), "\tjointGenotypeRun\n") =>
 	tmptimeLog = write(strcat(startmsg, endmsg));
 }
 
