615d06a2485cb1e98dc9311783a4b344f0db7bf7
zo0z <azzaea@uofk.edu>
2017-12-15 02:40:55 +0200

    style of companion site

--- docs/conf.py
+++ docs/conf.py
@@ -95,11 +95,11 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_context = {
-            'css_files': [
-                        '_static/theme_overrides.css',  # override wide tables in RTD theme
-                                ],
-                 }
+#html_context = {
+#            'css_files': [
+#                        '_static/theme_overrides.css',  # override wide tables in RTD theme
+#                                ],
+#                 }
 
 # -- Options for HTMLHelp output ------------------------------------------
 
