1c9a9e173cb9943a613a16e3c1edda47455207da
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-12-22 11:46:31 -0600

    Fix typo

--- test/BWTests/600Nodes_attempts/601Node.sh
+++ test/BWTests/600Nodes_attempts/601Node.sh
@@ -16,7 +16,7 @@ export CRAY_PPN=true
 export TURBINE_APP_RETRIES=6
 export ADLB_SERVERS=1
 export TURBINE_LOG=1    # This produces verbose logging info; great for debugging
-export ADBL_DEBUG_RANKS=1	# Displays layout of ranks and nodes
+export ADLB_DEBUG_RANKS=1	# Displays layout of ranks and nodes
 export TURBINE_OUTPUT=/scratch/sciteam/jacobrh/purge_exempt/Swift_testing/601Nodes/log_directory	# This specifies where the log info will be stored; defaults to one's home directory
 
 PATH=/u/sciteam/wozniak/Public/sfw/compute/swift-t/stc/bin:$PATH
