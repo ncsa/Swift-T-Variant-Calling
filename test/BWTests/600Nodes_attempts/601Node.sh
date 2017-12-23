664303b19d8c87ec8db880c90bae0e83640ddff1
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-12-23 15:17:56 -0600

    2nd attempt to make usable by others

--- test/BWTests/600Nodes_attempts/601Node.sh
+++ test/BWTests/600Nodes_attempts/601Node.sh
@@ -7,9 +7,10 @@ export PROCS=$(($PPN * $NODES))
 export WALLTIME=12:00:00
 export PROJECT=baib
 export QUEUE=high
+SCRATCH=/scratch/sciteam/$USER
 # export SWIFT_TMP=/scratch/sciteam/jacobrh/purge_exempt/Swift_testing/tmp
 # export TMPDIR=/scratch/sciteam/jacobrh/purge_exempt/Swift_testing/tmp
-export SWIFT_TMP=/scratch/sciteam/$USER/variant-tmp
+export SWIFT_TMP=$SCRATCH/variant-tmp
 export TMPDIR=$SWIFT_TMP
 
 # CRAY specific settings:
@@ -22,16 +23,22 @@ export TURBINE_LOG=1    # This produces verbose logging info; great for debuggin
 export ADLB_DEBUG_RANKS=1	# Displays layout of ranks and nodes
 # This specifies where the log info will be stored; defaults to one's home directory
 # export TURBINE_OUTPUT=/scratch/sciteam/jacobrh/purge_exempt/Swift_testing/601Nodes/log_directory
-export TURBINE_OUTPUT=$HOME/variant-output
+# Turbine will automatically create output directories here:
+export TURBINE_OUTPUT_ROOT=$SCRATCH/variant-output
+# Unique output directory numbers X001, X002, ...
 export TURBINE_OUTPUT_FORMAT="X%Q"
 
 PATH=/u/sciteam/wozniak/Public/sfw/compute/swift-t/stc/bin:$PATH
 
-THIS=$( dirname $0 )
+export THIS=$( dirname $0 ) # Also used by init.sh
 VARIANTS_HOME=$( cd $THIS/../../.. ; /bin/pwd )
+
+# Disable sync after STC on BlueWaters
+export SWIFT_T_SYNC=0
 
 swift-t -m cray -O3 -n $PROCS \
         -I $VARIANTS_HOME/src/ \
         -r $VARIANTS_HOME/src/bioapps \
+        -t i:./init.sh \
         $VARIANTS_HOME/src/VariantCalling.swift \
         -runfile=$THIS/601Node.runfile
---
+++ test/BWTests/600Nodes_attempts/init.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# INIT SH
+# This is executed by Swift/T before the job begins
+
+cp -v $THIS/601Node.runfile $TURBINE_OUTPUT
