6814174e586bc221c4e965bdd952bafaf3cad34e
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-12-23 09:14:45 -0600

    Attempt to make usable by others

--- test/BWTests/600Nodes_attempts/601Node.sh
+++ test/BWTests/600Nodes_attempts/601Node.sh
@@ -7,8 +7,10 @@ export PROCS=$(($PPN * $NODES))
 export WALLTIME=12:00:00
 export PROJECT=baib
 export QUEUE=high
-export SWIFT_TMP=/scratch/sciteam/jacobrh/purge_exempt/Swift_testing/tmp
-export TMPDIR=/scratch/sciteam/jacobrh/purge_exempt/Swift_testing/tmp
+# export SWIFT_TMP=/scratch/sciteam/jacobrh/purge_exempt/Swift_testing/tmp
+# export TMPDIR=/scratch/sciteam/jacobrh/purge_exempt/Swift_testing/tmp
+export SWIFT_TMP=/scratch/sciteam/$USER/variant-tmp
+export TMPDIR=$SWIFT_TMP
 
 # CRAY specific settings:
 export CRAY_PPN=true
@@ -18,11 +20,18 @@ export TURBINE_APP_RETRIES=6
 export ADLB_SERVERS=1
 export TURBINE_LOG=1    # This produces verbose logging info; great for debugging
 export ADLB_DEBUG_RANKS=1	# Displays layout of ranks and nodes
-export TURBINE_OUTPUT=/scratch/sciteam/jacobrh/purge_exempt/Swift_testing/601Nodes/log_directory	# This specifies where the log info will be stored; defaults to one's home directory
+# This specifies where the log info will be stored; defaults to one's home directory
+# export TURBINE_OUTPUT=/scratch/sciteam/jacobrh/purge_exempt/Swift_testing/601Nodes/log_directory
+export TURBINE_OUTPUT=$HOME/variant-output
+export TURBINE_OUTPUT_FORMAT="X%Q"
 
 PATH=/u/sciteam/wozniak/Public/sfw/compute/swift-t/stc/bin:$PATH
 
-swift-t -m cray -O3 -n $PROCS -o /scratch/sciteam/jacobrh/purge_exempt/Swift_testing/compiled.tic \
--I /projects/sciteam/baib/SwiftTesting/Swift-T-Variant-Calling/src/ -r /projects/sciteam/baib/SwiftTesting/Swift-T-Variant-Calling/src/bioapps \
-/projects/sciteam/baib/SwiftTesting/Swift-T-Variant-Calling/src/VariantCalling.swift -runfile=/scratch/sciteam/jacobrh/purge_exempt/Swift_testing/601Nodes/601Node.runfile
+THIS=$( dirname $0 )
+VARIANTS_HOME=$( cd $THIS/../../.. ; /bin/pwd )
 
+swift-t -m cray -O3 -n $PROCS \
+        -I $VARIANTS_HOME/src/ \
+        -r $VARIANTS_HOME/src/bioapps \
+        $VARIANTS_HOME/src/VariantCalling.swift \
+        -runfile=$THIS/601Node.runfile
