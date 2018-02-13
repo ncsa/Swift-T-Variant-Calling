09bec20e438a9a66b9cb108236473c830c36db3a
zo0z <azzaea@uofk.edu>
2018-02-13 11:42:37 -0600

    adding old xsede test files

--- test/XSEDE_stampede2/GIAB_example_config/runpipeline.sh
+++ test/XSEDE_stampede2/GIAB_example_config/runpipeline.sh
@@ -4,4 +4,4 @@ set -x
 
 scriptdir="/home1/04525/tg838247/swift_T_project/src/Swift-T-Variant-Calling"
 
-swift-t -m slurm -l -V -s settings.sh -u -I $scriptdir/src -r $scriptdir/src/bioapps $scriptdir/src/VariantCalling.swift -runfile=$PWD/GIAB_Garvan_NA12878_HG001_HiSeq_Exome.runfile | tee $PWD/log.swift.GIAB.run
+swift-t -m slurm -O3 -l -V -s settings.sh -u -I $scriptdir/src -r $scriptdir/src/bioapps $scriptdir/src/VariantCalling.swift -runfile=$PWD/GIAB_Garvan_NA12878_HG001_HiSeq_Exome.runfile | tee $PWD/log.swift.GIAB.run
--- test/XSEDE_stampede2/GIAB_example_config/settings.sh
+++ test/XSEDE_stampede2/GIAB_example_config/settings.sh
@@ -3,7 +3,6 @@ export PPN=3 #Number of processes per node
 export NODES=1
 export QUEUE=normal
 export WALLTIME=24:00:00
-export TURBINE_LOG=1
 export MAIL_ENABLED=1
 export MAIL_ADDRESS=azzaea@gmail.com
 #export ADLB_PERF_COUNTERS=1
@@ -11,8 +10,9 @@ export MAIL_ADDRESS=azzaea@gmail.com
 #export  ADLB_REPORT_LEAKS=1
 #export  ADLB_TRACE=true
 #export ADLB_DEBUG=true 
-export  ADLB_DEBUG_RANKS=1
-export SWIFT_TMP=/scratch/04525/tg838247/swift_T_project/GIAB_tmp_swift
-export TURBINE_APP_RETRIES=3
+export TURBINE_LOG=1
+export ADLB_DEBUG_RANKS=1
+export SWIFT_TMP=/scratch/04525/tg838247/swift_T_project/GIAB/tmp_swift
+export TURBINE_APP_RETRIES=5
 
 
