47d87f7820d02d7236d292a804131e516bca8a13
Matthew Kendzior <mkendzi2@iforge.ncsa.illinois.edu>
2018-02-13 11:23:02 -0600

    fixed logging rm bug

--- src/generalfunctions/general.swift
+++ src/generalfunctions/general.swift
@@ -63,10 +63,17 @@ app (void v) rm(file f) {
 () logging (string tmpdir, file timeLog, string toolDirName){
 	file tmplogs[] = glob(strcat(tmpdir, "/timinglogs/", toolDirName, "/*"));
 	if (size(tmplogs) > 0) {
-		append(timeLog, read(cat(tmplogs))) =>
-		foreach i in tmplogs {
+		append(timeLog, read(cat(tmplogs))); //=>
+
+		/*
+		These rm calls caused bugs
+		Since it is commented out, the TimingLog file will have redundant lines that need
+                to be filtered out
+
+		*/
+		/*foreach i in tmplogs {
 			rm(i);
-		}
+		}*/
 	}
 }
 
