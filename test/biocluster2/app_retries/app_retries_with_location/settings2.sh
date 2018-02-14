f04e66d22a9afe75322f4ddf5e0ed43574286b76
zo0z <azzaea@uofk.edu>
2018-02-14 13:07:53 -0600

    testing app-retries on different ranks-biocluster

---
+++ test/biocluster2/app_retries/app_retries_with_location/settings2.sh
@@ -0,0 +1,11 @@
+export PROCS=12 #total number of MPI processes
+export PPN=3 #Number of processes per node 
+export NODES=5
+export QUEUE=normal
+export WALLTIME=48:00:00
+export TURBINE_LOG=1
+export MAIL_ENABLED=1
+export MAIL_ADDRESS=azzaea@gmail.com
+export ADLB_DEBUG_RANKS=1
+export TURBINE_APP_RETRIES=3
+export SWIFT_TMP=/home/groups/hpcbio_shared/azza/swift_T_project/results/synthetic_data/tmp_swift
---
+++ test/biocluster2/app_retries/app_retries_with_location/t1_simple.swift
@@ -0,0 +1,21 @@
+import location;
+
+app () f (string input){
+	"false"
+}
+
+app () t () {
+	"true"
+}
+
+app () echo (string txt){
+	"/bin/echo" txt
+}
+
+@location=locationFromRank(3)
+t();
+
+@location=locationFromRank(4)
+f("hello");
+
+echo("hello");
