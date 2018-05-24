46c5ba5e8fae8dfff2a57625349b041a9ff296d3
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-05-24 12:11:44 -0500

    Simpler, more efficient logging()

---
+++ src/generalfunctions/logging.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -eu
+
+# UNUSED alternative implementation of logging()
+
+TMPDIR=$1
+TIMELOG=$2
+TOOLDIRNAME=$3
+
+TMPLOGS=$( ls $TMPDIR/timinglogs/$TOOLDIRNAME )
+if (( ${#TMPLOGS} ))
+then
+  cat $TMPLOGS >> $TIMELOG
+  rm -v $TMPLOGS
+fi
+echo "Updated $TIMELOG"
+
+# file tmplogs[] = glob(strcat(tmpdir, "/timinglogs/", toolDirName, "/*"));
+# if (size(tmplogs) > 0) {
+# 	append(timeLog, read(cat(tmplogs))); //=>
+
+# 	/*
+# 	These rm calls caused bugs
+# 	Since it is commented out, the TimingLog file will have redundant lines that need
+#         to be filtered out
+
+# 	*/
+# 	/*foreach i in tmplogs {
+# 		rm(i);
+# 	}*/
+# }
