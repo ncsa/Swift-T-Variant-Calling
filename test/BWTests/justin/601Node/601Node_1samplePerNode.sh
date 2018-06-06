044b7a66cfe7c268dac866e41463a3056b95b6bd
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-06-06 13:03:27 -0500

    New Swift/T and other script improvements

--- test/BWTests/justin/601Node/601Node_1samplePerNode.sh
+++ test/BWTests/justin/601Node/601Node_1samplePerNode.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
 export PPN=1
-export NODES=2
+export NODES=601
 export PROCS=$(($PPN * $NODES))
-export WALLTIME=00:05:00
-export PROJECT=baib
-# export QUEUE=high
-export QUEUE=debug
+export WALLTIME=00:10:00
+# export PROJECT=baib
+export PROJECT=bahk
+export QUEUE=high
+# export QUEUE=debug
 
-TEST=/scratch/sciteam/wozniak/601Node_tests
+SCRATCH=/scratch/sciteam/$USER
+DATA=$SCRATCH/601Node_tests
+
+echo SCRATCH=$SCRATCH
 
 # export SWIFT_TMP=/scratch/sciteam/jacobrh/purge_exempt/Swift_testing/tmp
 # export TMPDIR=/scratch/sciteam/jacobrh/purge_exempt/Swift_testing/tmp
-export SWIFT_TMP=$TEST/tmp
-export TMPDIR=$TEST/tmp
+export SWIFT_TMP=$DATA/tmp
+export TMPDIR=$DATA/tmp
 
 # CRAY specific settings:
 export CRAY_PPN=true
@@ -21,23 +25,26 @@ export CRAY_PPN=true
 # (Optional variables to set)
 export TURBINE_APP_RETRIES=6
 # export ADLB_SERVERS=0
-export TURBINE_LOG=0    # This produces verbose logging info; great for debugging
+export TURBINE_LOG=1    # This produces verbose logging info; great for debugging
 export ADLB_DEBUG_RANKS=1	# Displays layout of ranks and nodes
 
-export TURBINE_OUTPUT_ROOT=/scratch/sciteam/wozniak/turbine-output
+export TURBINE_OUTPUT_ROOT=$SCRATCH/turbine-output
 export TURBINE_OUTPUT_FORMAT=%Q
 
 # export TURBINE_OUTPUT=/scratch/sciteam/jacobrh/purge_exempt/Swift_testing/Justins_Testing/601Node_tests/1samplePerNode_log_directory	# This specifies where the log info will be stored; defaults to one's home directory
 
-PATH=/u/sciteam/wozniak/Public/sfw/compute/swift-t/stc/bin:$PATH
+# PATH=/u/sciteam/wozniak/Public/sfw/compute/swift-t/stc/bin:$PATH
+PATH=/u/sciteam/wozniak/Public/sfw/compute/swift-t-2018-06-05/stc/bin:$PATH
+which swift-t
 
-# STVC = Swift/T Variant Calling
-STVC=$HOME/proj/Variants
+# STVC = Swift/T Variant Calling main repo root
+STVC=$( readlink -f $( dirname $0 )/../../../.. )
 
-# /projects/sciteam/baib/SwiftTesting/Swift-T-Variant-Calling
+echo STVC=$STVC
 
 swift-t -m cray -O3 -n $PROCS \
 	-I $STVC/src/ \
 	-r $STVC/src/bioapps \
+        -e SWIFT_TMP \
 	$STVC/src/VariantCalling.swift \
-	-runfile=$TEST/601Node_1samplePerNode.runfile
+	-runfile=$STVC/test/BWTests/justin/601Node/601Node_1samplePerNode.runfile
