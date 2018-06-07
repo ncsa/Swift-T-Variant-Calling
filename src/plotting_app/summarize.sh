b296a7c7199ef0b54fbe5914d4e16422cdd7a019
azza <azzaea@uofk.edu>
2018-06-07 16:45:58 +0200

    fix path to TMPDIR in timinglog

--- src/plotting_app/summarize.sh
+++ src/plotting_app/summarize.sh
@@ -3,19 +3,21 @@
 #$1 is either a directory (of small timinglogs), or a timinglog.txt
 
 raw_input=$1 
-
+home=$(pwd)
+echo $home
 if [[ -d $raw_input ]]; then
     cd $raw_input
-    find . -name '*.txt' -exec cat {} \; > partial_run_timing.log
-    raw_logging_file=partial_run_timing.log
+    find . -name '*.log' -exec cat {} \; > $(pwd)/partial_run_timing.txt
+    raw_logging_file=$(pwd)/partial_run_timing.txt
+    cd $home
 
 elif [[ -f $raw_input ]]; then
     raw_logging_file=$raw_input
+
 else
     echo -e "The given arguement, **$raw_input** is NOT valid. \n\nPlease specify either:\n 1. A *Timing.log* file, or \n 2. The path to the *TMPDIR* of partial logs \n"
     echo "Exiting now!"
     exit 1
 fi
 
-echo 
 Rscript summarize.R $raw_logging_file
