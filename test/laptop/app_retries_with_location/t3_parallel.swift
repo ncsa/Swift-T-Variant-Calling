7571f449068d9ac25f3a3bf9cc0a6c829179eefb
zo0z <azzaea@uofk.edu>
2018-12-24 04:49:53 +0200

    add test files for app retries

---
+++ test/laptop/app_retries_with_location/t3_parallel.swift
@@ -0,0 +1,22 @@
+import location;
+
+app () f (string input){
+	"false"
+}
+
+@par app () t () {
+	"true"
+}
+
+app () echo (string txt){
+	"/bin/echo" txt
+}
+
+//@location=locationFromRank(3)
+@par=2
+t();
+
+@location=locationFromRank(4)
+f("hello");
+
+echo("hello");
